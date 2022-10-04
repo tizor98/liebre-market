11f653046f4f1403d046b807d0387d0828c746d2
Alberto Ortiz <baortiz98@outlook.com>
2022-10-03 20:37:34 -0500

    Register and login webpages added

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/index.html")));
 app.get("/new-product", (req, res) => res.sendFile(path.resolve(__dirname, "./views/addProduct.html")));
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
 
 app.listen(5000, () => console.log("Server initiated on port 5000"));
