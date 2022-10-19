487df2de39615dd17a432647f18ac3cbd2ad644c
Alberto Ortiz <baortiz98@outlook.com>
2022-10-18 23:16:50 -0500

    minor improvements

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
+const app = express();
 
 const main = require("./routes/main");
 const users = require("./routes/users");
 
-const app = express();
+app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.use("/", main);
 
 app.use("/user", users);
-
 
 const port = process.env.PORT || 5000;
 
