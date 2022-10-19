fd1b53e80c1359228f48a9a0c8f718828f2f7ee9
Alberto Ortiz <baortiz98@outlook.com>
2022-10-19 00:29:26 -0500

    Dockerfile added and minor fixes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 
 const mainRoutes = require("./routes/main");
 const usersRoutes = require("./routes/users");
 
+const app = express();
+
 app.set("view engine", "ejs");
 
-app.use(express.static(path.resolve(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", mainRoutes);
-
 app.use("/user", usersRoutes);
 
 const port = process.env.PORT || 5000;
